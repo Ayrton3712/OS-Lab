1b84dfcb8c537b9dd3df593a18fc45f197ab63b4
Ayrton3712 <mugittty@gmail.com>
2026-05-15 20:50:57 -0300

    Change arrays to std::vector

--- fcfs.cpp
+++ fcfs.cpp
@@ -1,28 +1,30 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main(){
-    int n, bt[10], wt[10], tat[10], i;
+    int n;
 
     cout << "Enter the number of processes: ";
     cin >> n;
 
+    vector<int> bt(n), wt(n), tat(n);
+
     cout << "Enter burst time:\n";
-    for (i = 0; i < n; i++){
+    for (int i = 0; i < n; i++){
         cin >> bt[i];
     }
 
-    wt[0] = 0; // First waiting time, this corresponds to the waiting time of the first process
+    wt[0] = 0;
 
-    // Calculating waiting times of the other processes
-    for (i = 1; i < n; i++){
-        wt[i] = wt[i - 1] + bt[i - 1]; // Waiting time for the current process = Waiting time of the previous process + Its burst time
+    for (int i = 1; i < n; i++){
+        wt[i] = wt[i - 1] + bt[i - 1];
     }
 
     cout << "\nProcess\tBurst\tWaiting\tTurnaround\n";
 
-    for (i = 0; i < n; i++){
+    for (int i = 0; i < n; i++){
         tat[i] = wt[i] + bt[i];
         cout << "P" << i + 1 << "\t" << bt[i] << "\t" << wt[i] << "\t" << tat[i] << endl;
     }
